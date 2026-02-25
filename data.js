0d7114e3f62b090fb455be911121116e29111905
Deadlywolf712 <johnttran94@gmail.com>
2026-02-25 00:39:08 -0500

    Fix video chronologies and sidebar sort order

--- data.js
+++ data.js
@@ -1,232 +1,70 @@
 const videoData = {
     "Kizomba Module 1 Technique Foundations": [
-        {
-            "title": "Lesson 1 Body Movement",
-            "driveId": "1qvpJEZTtMgDafrQWX5YKFGONPDShE52J"
-        },
-        {
-            "title": "Lesson 2 Connection Leading & Following",
-            "driveId": "1V8pX_ootISpsZd2j4ukTO-7mdhhRX0jW"
-        },
-        {
-            "title": "Lesson 3 Intro to Musicality",
-            "driveId": "1dKY43tPxIvHMj1FC_gzgUh02y5-kef1L"
-        },
-        {
-            "title": "Lesson 4 Floorcraft",
-            "driveId": "1dpr2XxZmPwoHFlD0Ub7oDgROU1ezmheo"
-        }
+        { title: "Lesson 1 Body Movement", driveId: "1qvpJEZTtMgDafrQWX5YKFGONPDShE52J" },
+        { title: "Lesson 2 Connection Leading & Following", driveId: "1V8pX_ootISpsZd2j4ukTO-7mdhhRX0jW" },
+        { title: "Lesson 3 Intro to Musicality", driveId: "1dKY43tPxIvHMj1FC_gzgUh02y5-kef1L" },
+        { title: "Lesson 4 Floorcraft", driveId: "1dpr2XxZmPwoHFlD0Ub7oDgROU1ezmheo" }
     ],
     "Kizomba Module 2 Technique Essentials": [
-        {
-            "title": "Lesson 5 Saida Technique",
-            "driveId": "1hzuV36oiYjMRJUJ21tYEzow1oTasstCr"
-        },
-        {
-            "title": "Lesson 6 Pivot Technique",
-            "driveId": "1I8kOj_VRLS9xF8gAzD0GA7bh-7W3jBS2"
-        },
-        {
-            "title": "Lesson 7 Tempo Changes",
-            "driveId": "1ormfqfHQVhLJTTyaU_kRp00WYtAif5wo"
-        },
-        {
-            "title": "Lesson 8 Dips and Balance Positions",
-            "driveId": "1jToFlLcwXsWW4tJwcfZYDLIENsktl74S"
-        }
+        { title: "Lesson 5 Saida Technique", driveId: "1hzuV36oiYjMRJUJ21tYEzow1oTasstCr" },
+        { title: "Lesson 6 Pivot Technique", driveId: "1I8kOj_VRLS9xF8gAzD0GA7bh-7W3jBS2" },
+        { title: "Lesson 7 Tempo Changes", driveId: "1ormfqfHQVhLJTTyaU_kRp00WYtAif5wo" },
+        { title: "Lesson 8 Dips and Balance Positions", driveId: "1jToFlLcwXsWW4tJwcfZYDLIENsktl74S" }
     ],
     "Kizomba Module 3 Dissociation": [
-        {
-            "title": "Lesson 9 Stopping",
-            "driveId": "1GasdXuqIeGxO5L2KZPpVW31j34Nyf2f8"
-        },
-        {
-            "title": "Lesson 10 Leg and Foot Slides",
-            "driveId": "13zROJqtZwaEf6GiaLazbcNKZiLMGtoKh"
-        },
-        {
-            "title": "Lesson 11 Footwork Variations",
-            "driveId": "1YLYYqZ1c1WbB3ME2a_hxGmuzpkagkbkX"
-        }
-    ],
-    "Kizomba Harmony - Level 2": [
-        {
-            "title": "Clockwise Hook Turn",
-            "driveId": "15xUv_NI5bE4ULEbE4EgK2WkWIhNkitvZ"
-        },
-        {
-            "title": "Clockwise Virgula",
-            "driveId": "12Myy67XHvS38TsJU2AAL5Ea6CmkEsj2A"
-        },
-        {
-            "title": "Degree Right Turn",
-            "driveId": "1NiiBO0J7nw9kC8zmsjcS92x7gbaPDfeZ"
-        },
-        {
-            "title": "Estrela 2",
-            "driveId": "1o_-uxj79XqwHOOgz3bi0AyAaeCZEtWYH"
-        },
-        {
-            "title": "Estrela 3",
-            "driveId": "1fwNeaEamG8hu4mZhI9MaBXG2ihXrn9E4"
-        },
-        {
-            "title": "Estrella 1",
-            "driveId": "1MjUMG28Soj2YOn8Dl7csa1rlujHMDopJ"
-        },
-        {
-            "title": "Level 2 Demo",
-            "driveId": "1zKjC7u5lfa7UQpgXpoNQDkgj-aqL6iyA"
-        },
-        {
-            "title": "Medium And Large Virgulas",
-            "driveId": "1orb65uF2dnkW6sJgnDXNJJRFXB5S-0NG"
-        },
-        {
-            "title": "Small Virgula",
-            "driveId": "17wrChRGFs5wrq5CdzuckL0WQZgvTPadw"
-        },
-        {
-            "title": "Stutter And Tap Steps",
-            "driveId": "1GITKxveNQnJhbSF0hTcUuISN-j6l8TRK"
-        },
-        {
-            "title": "Triangle Box Step",
-            "driveId": "1q2oHOpxo-8H6_l5tB500RaspIOGU3eZe"
-        },
-        {
-            "title": "Triangle Box Step Variation",
-            "driveId": "1t-zDRcFMsRbemzrOVoXHiZ0qk6NYtow0"
-        }
-    ],
-    "Kizomba Harmony - Follower": [
-        {
-            "title": "Ballance Step",
-            "driveId": "1D9KF9nOWwwNnKAHUjm3hl9urK2f5_Bbh"
-        },
-        {
-            "title": "Basic 2 Side Basic",
-            "driveId": "1INYfkk2HHVmmDmik-ud1fJszPT0q1q_m"
-        },
-        {
-            "title": "Cha Cha Step",
-            "driveId": "1biNuvjWP2K4R5WxCCk8hYHhBkKZlp8ry"
-        },
-        {
-            "title": "Degree Left Turn",
-            "driveId": "11Ar8ZphJobdL15jACe9yvO_hTmyuulgn"
-        },
-        {
-            "title": "Kizomba Walk",
-            "driveId": "1q9T2CTpcoU-WpHQeb6eVa0XxAt161983"
-        },
-        {
-            "title": "Tarraxinha Basic",
-            "driveId": "1xwJXYZ-mK1Ur6MiVTfhOdCDMBJ_wuDaF"
-        }
-    ],
-    "Kizomba Harmony - Level 3": [
-        {
-            "title": "Back And Forth Technique",
-            "driveId": "1cGkbyQ6IZLSgU0cN7YOrke4IBnEXIb3m"
-        },
-        {
-            "title": "Casamento",
-            "driveId": "1gXUvroswtKf4qi5hZ9_DsfzQylk1-lyo"
-        },
-        {
-            "title": "Circular Casamento",
-            "driveId": "19-SamXyG5KO2Wwd_80Lq6i4djwk-N9_T"
-        },
-        {
-            "title": "Circular Male And Female Saidas",
-            "driveId": "1Wsm4FkuEc0fbE7mP0MLA-cLNmnB3ar4i"
-        },
-        {
-            "title": "Female Saida",
-            "driveId": "191ZQL2_aD-X2kEN-w0pG_gIeaTAySbzB"
-        },
-        {
-            "title": "Female Saida With Cha Cha",
-            "driveId": "1BdyWI6iy969jrqaF-EyEj0deyEqKtCnT"
-        },
-        {
-            "title": "Lateral Casamento",
-            "driveId": "1MA37mSCjAhYWl4UM53kEfqw-c-ml8RYi"
-        },
-        {
-            "title": "Level 3 Demo",
-            "driveId": "1aSi6rJMipvYXkSFHiwePlNrwuEFcGdbj"
-        },
-        {
-            "title": "Male Saida",
-            "driveId": "12rBAvfXBG0gyKc8OvLKUPyNYaiHatwCB"
-        },
-        {
-            "title": "Male Saida Cross Body",
-            "driveId": "1G5rq569FywGjCWedq9_AvWEk8GH0laqH"
-        },
-        {
-            "title": "Male Saida Lateral Cross",
-            "driveId": "1tRcvRUIxtnQoa-x1AaxFkQ2FqhK71RXF"
-        },
-        {
-            "title": "Open Position Techniques Part 1",
-            "driveId": "1J5bM-xZOxw1P0kDhXBvo6Wm8PvmAAxO7"
-        },
-        {
-            "title": "Open Position Techniques Part 2",
-            "driveId": "1k0zerIyN8h2STL3o2GdbLnLuYO1NsW67"
-        }
+        { title: "Lesson 9 Stopping", driveId: "1GasdXuqIeGxO5L2KZPpVW31j34Nyf2f8" },
+        { title: "Lesson 10 Leg and Foot Slides", driveId: "13zROJqtZwaEf6GiaLazbcNKZiLMGtoKh" },
+        { title: "Lesson 11 Footwork Variations", driveId: "1YLYYqZ1c1WbB3ME2a_hxGmuzpkagkbkX" }
     ],
     "Kizomba Harmony - Level 1": [
-        {
-            "title": "90 Degree Left Turn",
-            "driveId": "1l_kvSeEZtwvGnIbJ8m7Nr6IiB4FmR_pW"
-        },
-        {
-            "title": "Basic 1 - Tarraxinha Basic",
-            "driveId": "1bgK8-EsNnAsEA0-u6bdpg9xzYI1JtPBy"
-        },
-        {
-            "title": "Basic 2 - Side Basic",
-            "driveId": "1icxGOcfKK00FvM0PtWMzURyglqEgv91Y"
-        },
-        {
-            "title": "Cha Cha Variations",
-            "driveId": "1lD0926il8KBjf_ZPvlCoj4GR_d5iNGKA"
-        },
-        {
-            "title": "Introduction",
-            "driveId": "1ED0WG_GuIb8yUD4bdYQNLAookDFW0bvC"
-        },
-        {
-            "title": "Level 1 Demo",
-            "driveId": "1H7iU9NkoT1RvIJN1wHNagUWY08chLERp"
-        },
-        {
-            "title": "Rotational Walking",
-            "driveId": "1WQVP-NhwsAFeRDdil4YarWK-PQPQvXs_"
-        },
-        {
-            "title": "Thank You",
-            "driveId": "1Q6UjmnztpNAIr61W5SBDycbLD-XaUj1n"
-        },
-        {
-            "title": "The Balance Step",
-            "driveId": "1lB9G5Pur-FncaNP7DZ0fiHcWaiUZlicE"
-        },
-        {
-            "title": "The Cha Cha Step",
-            "driveId": "1XlzjCovoo-cl4uBtMzz_BhLzIczYfOPh"
-        },
-        {
-            "title": "The Kizomba Connection",
-            "driveId": "11NVXjV_QqQqPVetKP8t6P2_Fs56TriYR"
-        },
-        {
-            "title": "The Kizomba Walk",
-            "driveId": "1PbdPL_lgBm0rvqH9ibBGC4no2iJMS6OD"
-        }
+        { title: "Introduction", driveId: "1ED0WG_GuIb8yUD4bdYQNLAookDFW0bvC" },
+        { title: "The Kizomba Connection", driveId: "11NVXjV_QqQqPVetKP8t6P2_Fs56TriYR" },
+        { title: "Basic 1 - Tarraxinha Basic", driveId: "1bgK8-EsNnAsEA0-u6bdpg9xzYI1JtPBy" },
+        { title: "Basic 2 - Side Basic", driveId: "1icxGOcfKK00FvM0PtWMzURyglqEgv91Y" },
+        { title: "The Kizomba Walk", driveId: "1PbdPL_lgBm0rvqH9ibBGC4no2iJMS6OD" },
+        { title: "The Balance Step", driveId: "1lB9G5Pur-FncaNP7DZ0fiHcWaiUZlicE" },
+        { title: "90 Degree Left Turn", driveId: "1l_kvSeEZtwvGnIbJ8m7Nr6IiB4FmR_pW" },
+        { title: "The Cha Cha Step", driveId: "1XlzjCovoo-cl4uBtMzz_BhLzIczYfOPh" },
+        { title: "Cha Cha Variations", driveId: "1lD0926il8KBjf_ZPvlCoj4GR_d5iNGKA" },
+        { title: "Level 1 Demo", driveId: "1H7iU9NkoT1RvIJN1wHNagUWY08chLERp" },
+        { title: "Rotational Walking", driveId: "1WQVP-NhwsAFeRDdil4YarWK-PQPQvXs_" },
+        { title: "Thank You", driveId: "1Q6UjmnztpNAIr61W5SBDycbLD-XaUj1n" }
+    ],
+    "Kizomba Harmony - Level 2": [
+        { title: "90 Degree Right Turn", driveId: "1NiiBO0J7nw9kC8zmsjcS92x7gbaPDfeZ" },
+        { title: "Clockwise Hook Turn", driveId: "15xUv_NI5bE4ULEbE4EgK2WkWIhNkitvZ" },
+        { title: "Estrella 1", driveId: "1MjUMG28Soj2YOn8Dl7csa1rlujHMDopJ" },
+        { title: "Estrela 2", driveId: "1o_-uxj79XqwHOOgz3bi0AyAaeCZEtWYH" },
+        { title: "Estrela 3", driveId: "1fwNeaEamG8hu4mZhI9MaBXG2ihXrn9E4" },
+        { title: "Triangle Box Step", driveId: "1q2oHOpxo-8H6_l5tB500RaspIOGU3eZe" },
+        { title: "Triangle Box Step Variation", driveId: "1t-zDRcFMsRbemzrOVoXHiZ0qk6NYtow0" },
+        { title: "Stutter And Tap Steps", driveId: "1GITKxveNQnJhbSF0hTcUuISN-j6l8TRK" },
+        { title: "Small Virgula", driveId: "17wrChRGFs5wrq5CdzuckL0WQZgvTPadw" },
+        { title: "Medium And Large Virgulas", driveId: "1orb65uF2dnkW6sJgnDXNJJRFXB5S-0NG" },
+        { title: "Clockwise Virgula", driveId: "12Myy67XHvS38TsJU2AAL5Ea6CmkEsj2A" },
+        { title: "Level 2 Demo", driveId: "1zKjC7u5lfa7UQpgXpoNQDkgj-aqL6iyA" }
+    ],
+    "Kizomba Harmony - Level 3": [
+        { title: "Open Position Techniques Part 1", driveId: "1J5bM-xZOxw1P0kDhXBvo6Wm8PvmAAxO7" },
+        { title: "Open Position Techniques Part 2", driveId: "1k0zerIyN8h2STL3o2GdbLnLuYO1NsW67" },
+        { title: "Back And Forth Technique", driveId: "1cGkbyQ6IZLSgU0cN7YOrke4IBnEXIb3m" },
+        { title: "Male Saida", driveId: "12rBAvfXBG0gyKc8OvLKUPyNYaiHatwCB" },
+        { title: "Female Saida", driveId: "191ZQL2_aD-X2kEN-w0pG_gIeaTAySbzB" },
+        { title: "Female Saida With Cha Cha", driveId: "1BdyWI6iy969jrqaF-EyEj0deyEqKtCnT" },
+        { title: "Circular Male And Female Saidas", driveId: "1Wsm4FkuEc0fbE7mP0MLA-cLNmnB3ar4i" },
+        { title: "Casamento", driveId: "1gXUvroswtKf4qi5hZ9_DsfzQylk1-lyo" },
+        { title: "Circular Casamento", driveId: "19-SamXyG5KO2Wwd_80Lq6i4djwk-N9_T" },
+        { title: "Lateral Casamento", driveId: "1MA37mSCjAhYWl4UM53kEfqw-c-ml8RYi" },
+        { title: "Male Saida Cross Body", driveId: "1G5rq569FywGjCWedq9_AvWEk8GH0laqH" },
+        { title: "Male Saida Lateral Cross", driveId: "1tRcvRUIxtnQoa-x1AaxFkQ2FqhK71RXF" },
+        { title: "Level 3 Demo", driveId: "1aSi6rJMipvYXkSFHiwePlNrwuEFcGdbj" }
+    ],
+    "Kizomba Harmony - Follower": [
+        { title: "Tarraxinha Basic", driveId: "1xwJXYZ-mK1Ur6MiVTfhOdCDMBJ_wuDaF" },
+        { title: "Basic 2 Side Basic", driveId: "1INYfkk2HHVmmDmik-ud1fJszPT0q1q_m" },
+        { title: "90 Degree Left Turn", driveId: "11Ar8ZphJobdL15jACe9yvO_hTmyuulgn" },
+        { title: "Kizomba Walk", driveId: "1q9T2CTpcoU-WpHQeb6eVa0XxAt161983" },
+        { title: "Ballance Step", driveId: "1D9KF9nOWwwNnKAHUjm3hl9urK2f5_Bbh" },
+        { title: "Cha Cha Step", driveId: "1biNuvjWP2K4R5WxCCk8hYHhBkKZlp8ry" }
     ]
 };
